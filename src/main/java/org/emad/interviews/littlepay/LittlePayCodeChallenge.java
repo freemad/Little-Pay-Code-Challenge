6b8d6710657304bcc55b63a3fe401d47cf39b864
Emad Mokhatab <e.mokhotab@fwutech.com>
2023-08-02 08:25:01 +0330

    Added main services calls and the results CSV files

--- src/main/java/org/emad/interviews/littlepay/LittlePayCodeChallenge.java
+++ src/main/java/org/emad/interviews/littlepay/LittlePayCodeChallenge.java
@@ -1,7 +1,29 @@
 package org.emad.interviews.littlepay;
 
+import org.emad.interviews.littlepay.entities.Tap;
+import org.emad.interviews.littlepay.exceptions.BusinessException;
+import org.emad.interviews.littlepay.services.StopService;
+import org.emad.interviews.littlepay.services.TapService;
+import org.emad.interviews.littlepay.services.TripService;
+
+import java.util.List;
+
 public class LittlePayCodeChallenge {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hi Examiner! :)");
+
+        StopService stopService = new StopService();
+        stopService.updateStops();
+        TapService tapService = new TapService();
+        TripService tripService = new TripService();
+        try {
+            String tapsFile = args[0];
+            String tripsFile = args[1];
+            List<Tap> taps = tapService.readTaps(tapsFile);
+            tripService.calculateTrips(taps);
+            tripService.writeTrips(tripsFile);
+        } catch (BusinessException e) {
+            e.printStackTrace();
+        }
     }
 }
